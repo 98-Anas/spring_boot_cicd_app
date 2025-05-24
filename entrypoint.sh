fda904d9b7611bcafee65e8613c24f74780e6218
98-Anas <algalad.anas@gmail.com>
2025-05-24 20:50:54 +0300

    Fixing scan stage

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start GitLab runner in background
-/usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner &
-
-# Start Spring Boot application
-java -jar /app/app.jar
