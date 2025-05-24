5266cece8870ad5c55cd0e110aa8e3d2c5e150fb
98-Anas <algalad.anas@gmail.com>
2025-05-24 15:55:54 +0300

    Fixing gitlab runner

--- run.sh
+++
@@ -1,5 +0,0 @@
-docker build -t spring-boot-gitlab-runner .
-docker run -d --name gitlab-runner --restart always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
-  spring-boot-gitlab-runner
